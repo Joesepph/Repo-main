bb114447514296ae36332b1f02f30c0dadbd3449
Joesepph <mnmanpolish@gmail.com>
2022-03-02 10:08:44 +0100

    Algorytm

---
+++ Algorytm liczb pierwszych/Algorytm liczb pierwszych.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+
+	int lp = 0;
+	int p = 2;
+	int d = 1;
+
+	cout << "Ile liczb pierwszych chcesz wyswietlic";  cin >> n;
+
+	while (lp < n) {
+		for (d = 1; d < p - 1; d++) {
+			if (p % d == 0) {
+				cout << p << ", ";
+			}
+		}
+		lp++;
+		p++;
+	}
+}
