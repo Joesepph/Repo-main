53136c594d06e5b002622dab42e3e7bc0923ac4e
Joesepph <mnmanpolish@gmail.com>
2022-03-16 10:04:05 +0100

    Update

---
+++ Obliczanie_NWW_i_NWD/Obliczanie_NWW_i_NWD.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+
+	cout << "Podaj pierwszą liczbę: "; cin >> a;
+	cout << "Podaj drugą liczbę: "; cin >> b; cout << "\n";
+
+	int iloczyn = a * b;
+
+	while (a != b) {
+		if (a < b) {
+			b = b - a;
+		}
+		else {
+			a = a - b;
+		}
+	}
+	int nww = iloczyn / a;
+
+	cout << "Najwiekszy wspolny dzielnik: " << a << "\n";
+	cout << "Najmniejsza wspolna wielokrotnosc: " << nww << "\n";
+}
